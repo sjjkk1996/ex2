28c03a15bfe309862cee9fca3cec53c49546ab94
jjcdesk <sjjkk1996>
2021-08-26 03:01:45 +0900

    0825

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,8 +1,45 @@
 package org.zerock.ex2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.ex2.entity.Memo;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import javax.transaction.Transactional;
+import java.util.List;
 
+public interface MemoRepository<Lsit> extends JpaRepository<Memo, Long> {
+    List<Memo> findByMnoBetweenOrderByMnoDesc( Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteByMnoLessThan(Long mno);
+
+    @Query("select m from Memo m where m.mno > 30 and m.mno < 40 order by m.mno desc")
+    List<Memo> getListDesc();
+
+    @Transactional
+    @Modifying
+    @Query("update Memo m set m.memoText = :memoText where m.mno = :mno ")
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+    @Transactional
+    @Modifying
+    @Query("update Memo m set m.memoText = :#{#param.memoText} where m.mno = :#{#param.mno} ")
+    int updateMemoObject(@Param("param") Memo memo);
+
+    @Query(value="select m from Memo m where m.mno > :mno",
+            countQuery = "select count(m) from Memo m where m.mno > :mno")
+    Page<Memo> getListWithQuery(Long mno, Pageable pageable);
+
+    @Query(value="select m.mno, m.memoText, CURRENT_DATE from Memo m " +
+            " where m.mno > :mno",
+            countQuery = "select count(m) from Memo m where m.mno > :mno")
+    Page<Object[]> getListWithQueryObject(Long mno, Pageable pageable);
+
+    @Query(value="select * from memo where mno>70 ", nativeQuery = true)
+    List<Object[]> getNativeResult();
 }
--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,26 +3,156 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.zerock.ex2.entity.Memo;
 
+import javax.print.attribute.standard.PageRanges;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemoRepositoryTests {
 
-    @Autowired //자동으로 들고와서 인스턴스를 만들어줌 spring 빈이있기때문임
+    @Autowired
     MemoRepository memoRepository;
 
     @Test
-    public void testClass(){
+    public void testClase() {
         System.out.println("testClass::"+memoRepository.getClass().getName());
     }
+//    @Test
+//    public void testInsert() {
+//        IntStream.rangeClosed(1,100).forEach(i -> {
+//            Memo memo = Memo.builder().memoText("Sample..."+i).build();
+//            memoRepository.save(memo);
+//        });
+//    }
+//    @Test
+//    public void testSelect() {
+//        Long mno = 100L;
+//        Optional<Memo> result = memoRepository.findById(mno);
+//        System.out.println("===========================");
+//        if (result.isPresent()) {
+//            Memo memo = result.get();
+//            System.out.println(memo);
+//        } else {
+//            System.out.println("존재하지 않는 엔티티.");
+//        }
+//    }
+//    @Transactional
+//    @Test
+//    public void testSelect2() {
+//        Long mno = 100L;
+//        Memo memo = memoRepository.getOne(mno);
+//        System.out.println("===========================");
+//        System.out.println(memo);
+//    }
+//    @Test
+//    public void testUpdate() {
+//        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+//        System.out.println(memoRepository.save(memo));
+//    }
+//    @Test
+//    public void testDelete() {
+//        Long mno = 100L;
+//        memoRepository.deleteById(mno);
+//    }
+//    @Test
+//    public void testPageDefault() {
+//        Pageable pageable = PageRequest.of(10, 10);
+//        Page<Memo> result = memoRepository.findAll(pageable);
+//        System.out.println("result >> "+result);
+//        System.out.println("===========================");
+//        System.out.println("Total Pages : "+result.getTotalPages());//총몇페이지
+//        System.out.println("Total Count : "+result.getTotalElements());//전체개수
+//        System.out.println("Page Number : "+result.getNumber());//현재 페이지 번호 0부터 시작
+//        System.out.println("Page Size : "+result.getSize());//페이지당 데이터 개수
+//        System.out.println("has next page? : "+result.hasNext());//다음 페이지 존재여부
+//        System.out.println("First page? : "+result.isFirst());//시작페이지 (0) 여부
+//        System.out.println("---------------------------");
+//        for (Memo memo : result.getContent()) {
+//            System.out.println(memo);
+//        }
+//    }
+//    @Test
+//    public void testSort() {
+//        Sort sort1 = Sort.by("mno").descending();
+//        Pageable pageable = PageRequest.of(0,10,sort1);
+//        Page<Memo> result = memoRepository.findAll(pageable);
+//        result.get().forEach(memo -> {
+//            System.out.println(memo);
+//        });
+//    }
+//    @Test
+//    public void testQueryMethods() {
+//        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+//        for (Memo memo : list) {
+//            System.out.println(memo);
+//        }
+//    }
+//    @Test
+//    public void testQueryMethodWithPageable() {
+//        Pageable pageable = PageRequest.of(0,10,Sort.by("mno").descending());
+//        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+//        result.get().forEach(memo -> System.out.println(memo));
+//    }
+
+//    @Commit
+//    @Transactional
+//    @Test
+//    void testDeleteQueryMathods() {
+//        memoRepository.deleteByMnoLessThan(20L);
+//    }
+
+//    @Test
+//    void testGetListDesc() {
+//        List<Memo> list = memoRepository.getListDesc();
+//        for (Memo memo : list) {
+//            System.out.println(memo);
+//        }
+//    }
+
+//    @Test
+//    void testUpdateMemoText() {
+//        memoRepository.updateMemoText(50L,"change");
+//    }
+//
+//    @Test
+//    void testUpdateMemoObject(){
+//        Memo memo = Memo.builder().mno(50L).memoText("Changed1").build();
+//        memoRepository.updateMemoObject(memo);
+//    }
+//
     @Test
-    public void testInsert() {
-        IntStream.rangeClosed(1,100).forEach(i -> {
-            Memo memo = Memo.builder().memoText("Sample..."+i).build();
-            //Memo.builder()는 생성자 만드는거임
-            memoRepository.save(memo); //save가 insert 해주는거임 여기서는 sql을 안씀 여기서는
-        });
+    void testGetListWithQuery() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.getListWithQuery(20L, pageable);
+        result.get().forEach(memo -> System.out.println(memo));
     }
+
+
+
+//    @Test
+//    void testGetListWithQueryObject() {
+//        Pageable pageable = PageRequest.of(0,10, Sort.by("mno").descending());
+//        Page<Object[]> result = memoRepository.getListWithQueryObject(20L, pageable);
+//        result.get().forEach(objects -> System.out.println(
+//                objects[0] + "/"+ objects[1] + "/" + objects[2]));
+//    }
+//
+//    @Test
+//    void testGetNativeResult(){
+//        List<Object[]> result = memoRepository.getNativeResult();
+//        for (Object[] objects : result){
+//            System.out.println(objects[0] + "/" + objects[1]);
+//        }
+//    }
+
+
 }
